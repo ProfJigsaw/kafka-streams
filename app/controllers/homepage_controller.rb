3fc35370d4742a3ba49a91d8ca94eba5d058b511
ProfJigsaw <dhagodfather@gmail.com>
2019-03-24 21:38:38 +0100

    Incorporate chark kick

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,9 @@
 class HomepageController < ApplicationController
   def index
     @country_stats = CountryStat.order('visit_count desc')
-    @total_visit_count = CountryStat.sum(:visit_count)
-    @max_visit_count = CountryStat.maximum(:visit_count)
+    @map = {}
+    @country_stats.each do |country_data|
+      @map[country_data.country_code] = country_data.visit_count
+    end
   end
 end
